fd3aaee600f02b71719ec325aee098eb1541971c
Konstantin <pilot911@yandex.ru>
2016-06-25 20:15:04 +0300

    New commit

--- resources/package-process/custom.php
+++ resources/package-process/custom.php
@@ -1,7 +1,7 @@
 <?php
 
-    \App\Vendor\Telenok\Core\Support\Install\Custom::recursiveCopy(__DIR__ . "/../app", __DIR__ . "/../../../../app/Vendor");
-    \App\Vendor\Telenok\Core\Support\Install\Custom::addListener('\App\Vendor\Telenok\Account\Event\Listener');
-    \App\Vendor\Telenok\Core\Support\Install\Custom::addServiceProvider('\App\Vendor\Telenok\Account\CoreServiceProvider');
+    \Telenok\Core\Support\Install\Custom::recursiveCopy(__DIR__ . "/../app", __DIR__ . "/../../../../../app/Vendor");
+    \Telenok\Core\Support\Install\Custom::addListener('\App\Vendor\Telenok\Account\Event\Listener');
+    \Telenok\Core\Support\Install\Custom::addServiceProvider('\App\Vendor\Telenok\Account\CoreServiceProvider');
 
 
