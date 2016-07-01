54c680e9ec52761b1c19afe58cb89b0da63d025c
Konstantin <pilot911@yandex.ru>
2016-07-01 22:46:00 +0300

    New commit

--- resources/package-process/composer-subscript.2000.php
+++ resources/package-process/composer-subscript.2000.php
@@ -2,6 +2,6 @@
 
     \Telenok\Core\Support\Install\ComposerScripts::recursiveCopy(__DIR__ . "/../app", __DIR__ . "/../../../../../app/Vendor");
     \Telenok\Core\Support\Install\ComposerScripts::addListener('\App\Vendor\Telenok\News\Event\Listener');
-    \Telenok\Core\Support\Install\ComposerScripts::addServiceProvider('\App\Vendor\Telenok\News\CoreServiceProvider');
+    \Telenok\Core\Support\Install\ComposerScripts::addServiceProvider('\App\Vendor\Telenok\News\NewsServiceProvider');
 
 
