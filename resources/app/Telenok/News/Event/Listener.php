873d632a711a70f7305bc471bd54c0abf831eb45
Konstantin <pilot911@yandex.ru>
2016-09-17 21:41:39 +0300

    New commit

--- resources/app/Telenok/News/Event/Listener.php
+++ resources/app/Telenok/News/Event/Listener.php
@@ -1,25 +1,4 @@
 <?php namespace App\Vendor\Telenok\News\Event;
 
 class Listener extends \Telenok\News\Event\Listener {
-
-
-    public function onRepositoryPackage(RepositoryPackage $event)
-    {
-        $event->getList()->push('Telenok\News\PackageInfo');
-    }
-
-    public function subscribe($events)
-    {
-        $this->addListenerRepositoryPackage($events);
-    }
-
-    public function addListenerRepositoryPackage($events)
-    {
-        $events->listen(
-            'Telenok\Core\Event\RepositoryPackage',
-            'App\Vendor\Telenok\News\Event\Listener@onRepositoryPackage'
-        );
-    }
-
-
 }
--- src/Telenok/News/Event/Listener.php
+++ src/Telenok/News/Event/Listener.php
@@ -1,10 +1,20 @@
 <?php namespace Telenok\News\Event;
 
+use Telenok\Core\Event\RepositoryPackage;
+
 class Listener {
 
-    public function onRepositoryPackage($event) {}
+    public function onRepositoryPackage(RepositoryPackage $event)
+    {
+        $event->getList()->push('Telenok\News\PackageInfo');
+    }
 
     public function subscribe($events)
+    {
+        $this->addListenerRepositoryPackage($events);
+    }
+
+    public function addListenerRepositoryPackage($events)
     {
         $events->listen(
             'Telenok\Core\Event\RepositoryPackage',
